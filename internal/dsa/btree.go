8e1a7242de71eb47852185101dedbfc85fe2425e
delawer33 <shukry.aliev@mail.ru>
2026-01-30 11:31:22 +0300

    initial leaf logic

--- internal/dsa/btree.go
+++ internal/dsa/btree.go
@@ -99,7 +99,7 @@ func (node BN) nbytes() uint16 {
 	return node.kvPos(node.nkeys())
 }
 
-// Find first key less than or equal to given
+// Find first key less than or equal to given TODO: binary search
 func nodeLookupLE(node BN, key []byte) uint16 {
 	nkeys := node.nkeys()
 	found := uint16(0)
@@ -114,3 +114,55 @@ func nodeLookupLE(node BN, key []byte) uint16 {
 	}
 	return found
 }
+
+// leaves
+
+func leafInsert(new BN, old BN, idx uint16, key []byte, val []byte) {
+	new.setHeader(BN_LEAF, old.nkeys() + 1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKV(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx, old.nkeys()-idx)	
+}
+
+func nodeAppendKV(new BN, idx uint16, ptr uint64, key []byte, val []byte) {
+	new.setPtr(idx, ptr)
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new[pos+0:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new[pos+2:], uint16(len(val)))
+	copy(new[pos+4:], key)
+	copy(new[pos+4+uint16(len(key)):], val)
+	new.setOffset(idx+1, new.getOffset(idx)+4+uint16(len(key)+len(val)))
+}
+
+func nodeAppendRange(new BN, old BN, dstNew uint16, srcOld uint16, n uint16) {
+	assert(srcOld+n <= old.nkeys())
+	assert(dstNew+n <= new.nkeys())
+	if n == 0 {
+		return
+	}
+	for i := uint16(0); i < n; i++ {
+		new.setPtr(dstNew+i, old.getPtr(srcOld+i))
+	}
+	dstBegin := new.getOffset(dstNew)
+	srcBegin := old.getOffset(srcOld)
+	for i := uint16(1); i <= n; i++ {
+		offset := dstBegin + old.getOffset(srcOld+i) - srcBegin
+		new.setOffset(dstNew+i, offset)
+	}
+	start := old.kvPos(srcOld)
+	end := old.kvPos(srcOld + n)
+	copy(new[new.kvPos(dstNew):], old[start:end])
+}
+
+func nodeReplaceKidN(tree *BT, new BN, old BN, idx uint16, kids ...BN) {
+	inc := uint16(len(kids))
+	new.setHeader(BN_NODE, old.nkeys()+inc-1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	for i, node := range kids {
+		nodeAppendKV(new, idx+uint16(i), tree.new(node), node.getKey(0), nil)
+	}
+	nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-idx-1)
+}
+
+
+
